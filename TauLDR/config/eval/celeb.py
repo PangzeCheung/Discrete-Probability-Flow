73e5f55de23ca298f0a5f5dcecba13b6677e81a3
PangzeCheung <37894893+PangzeCheung@users.noreply.github.com>
2024-01-08 11:41:14 +0800

    Create celeb.py

---
+++ TauLDR/config/eval/celeb.py
@@ -0,0 +1,44 @@
+import ml_collections
+
+def get_config():
+
+    datasets_folder = 'path/to/datasets'
+    model_location = './models/celeb_128/checkpoints/ckpt_0000392000.pt'
+    model_config_location = './models/celeb_128/config/config_001.yaml'
+
+    config = ml_collections.ConfigDict()
+    config.eval_name = 'CIFAR10'
+    config.train_config_overrides = [
+        [['device'], 'cpu'],
+        [['data', 'root'], datasets_folder],
+        [['distributed'], False]
+    ]
+    config.train_config_path = model_config_location
+    config.checkpoint_path = model_location
+
+    config.device = 'cpu'
+
+    config.data = data = ml_collections.ConfigDict()
+    data.name = 'DiscreteCIFAR10'
+    data.root = datasets_folder
+    data.train = True
+    data.download = True
+    data.S = 256
+    data.batch_size = 16
+    data.shuffle = True
+    data.shape = [3,128,128]
+    data.random_flips = False
+
+    config.sampler = sampler = ml_collections.ConfigDict()
+    sampler.name = 'TauLeaping' # TauLeaping or PCTauLeaping
+    sampler.num_steps = 1000
+    sampler.min_t = 0.01
+    sampler.eps_ratio = 1e-9
+    sampler.initial_dist = 'gaussian'
+    sampler.num_corrector_steps = 10
+    sampler.corrector_step_size_multiplier = 1.5
+    sampler.corrector_entry_time = 0.1
+    sampler.DPF_type = 1
+    config.DPF_type = sampler.DPF_type
+
+    return config
